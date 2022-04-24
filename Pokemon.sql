630310086af2e391798a776fabcf2f384dc6b4e7
medha-shah <83974584+medha-shah@users.noreply.github.com>
2022-04-24 12:28:02 -0400

    Update Pokemon.sql

--- Pokemon.sql
+++ Pokemon.sql
@@ -1,6 +1,7 @@
 use pokemon_server;
 
-SELECT * FROM pokemons;
+SELECT * FROM pokemons WHERE `#` = 4;
+
 
 CREATE TABLE user
 (
@@ -16,6 +17,13 @@ name VARCHAR(32) NOT NULL,
 FOREIGN KEY (userId) REFERENCES user(userId)
 	ON UPDATE RESTRICT ON DELETE RESTRICT
 );
+
+CREATE TABLE type
+(
+name VARCHAR(32) PRIMARY KEY NOT NULL,
+advantages VARCHAR(64) NOT NULL,
+disadvantages VARCHAR(64) NOT NULL
+);
     
 CREATE TABLE pokemon
 (
@@ -25,13 +33,6 @@ generation INT NOT NULL,
 type VARCHAR(32) NOT NULL,
 FOREIGN KEY (type) REFERENCES type(name)
 	ON UPDATE RESTRICT ON DELETE RESTRICT
-);
-
-CREATE TABLE type
-(
-name VARCHAR(32) PRIMARY KEY NOT NULL,
-advantages VARCHAR(64) NOT NULL,
-disadvantages VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE powers
@@ -65,15 +66,10 @@ FOREIGN KEY (pId) REFERENCES pokemon(pId)
 FOREIGN KEY (item) REFERENCES item(name)
 	ON UPDATE CASCADE ON DELETE RESTRICT
 );
-
-CREATE TABLE pokeball
-(
-name VARCHAR(32) PRIMARY KEY NOT NULL,
-catchRateBoost INT NOT NULL
-);
     
 CREATE TABLE team
 (
+teamId INT auto_increment PRIMARY KEY,
 teamMember1 INT NOT NULL,
 teamMember2 INT,
 teamMember3 INT,
@@ -84,9 +80,46 @@ teamMember6 INT
 
 CREATE TABLE battle
 (
-player INT,
-opponent INT,
-location VARCHAR(32)
+playerTeam INT,
+opponentTeam INT,
+location VARCHAR(32),
+FOREIGN KEY (playerTeam) REFERENCES team(teamId)
+	ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (opponentTeam) REFERENCES team(teamId)
+	ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
+-- adds a teammember to team
+DELIMITER //
+CREATE procedure addTeamMember(teammemberId INT)
+BEGIN
+INSERT INTO team (pId) VALUES (teammemberId);
+END//
 
+-- deletes a teammember from team
+DELIMITER //
+CREATE procedure deleteTeamMember(teammemberId INT)
+BEGIN
+DELETE FROM team WHERE teammember.pId = teammemberId;
+END //
+
+-- update a teammember from team
+DELIMITER //
+CREATE procedure updateTeamMember(teammemberId INT, field VARCHAR(32), newValue INT)
+BEGIN
+UPDATE teammember SET field = newValue WHERE teammember.pId = teammemberId;
+END //
+
+-- compares type advantages for 2 given teams
+DELIMITER //
+CREATE procedure compare (IN teamId1 INT, teamId2 INT)
+BEGIN
+SELECT teamMember1, teamMember2, teamMember3, teamMember4, teamMember5, teamMember6 FROM team
+INNER JOIN
+WHERE teamId = 456,
+teamMember
+END//
+
+-- If less than 6 members, reccomends a team member to add
+
+function to read pokedex -- find by ID AND NAME
